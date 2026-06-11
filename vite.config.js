fab358d9c75932083c4af56fe6d05911955ae1ff
Daniel Systems <tiri18b@gmail.com>
2026-06-11 04:27:05 +0300

    add legacy webview support for tv apk

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+﻿import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    legacy({
+      targets: [
+        "defaults",
+        "Android >= 5",
+        "Chrome >= 49"
+      ],
+      modernPolyfills: true,
+      renderLegacyChunks: true
+    })
+  ],
+  build: {
+    target: "es2015",
+    cssTarget: "chrome49"
+  }
+});
