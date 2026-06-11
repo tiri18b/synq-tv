97d049bc4a1da9978d097aa446ff27bcb1f41757
Daniel Systems <tiri18b@gmail.com>
2026-06-11 04:01:27 +0300

    move tv apps button to far right

---
+++ scripts/move_apps_button_right.cjs
@@ -0,0 +1,19 @@
+﻿const fs = require("fs");
+
+const cssPath = "src/pages/TV.css";
+
+let css = fs.readFileSync(cssPath, "utf8");
+
+css += `
+
+/* MOVE APPS BUTTON TO FAR RIGHT */
+.client-tv-home-button {
+  left: auto !important;
+  right: 1.3vw !important;
+  top: 1.5vh !important;
+}
+`;
+
+fs.writeFileSync(cssPath, css, "utf8");
+
+console.log("Apps button moved to far right");
